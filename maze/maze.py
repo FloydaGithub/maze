d49d46815e68bc5ae5ad42b1d0b532b53506545d
Floyda <floyda@163.com>
2016-03-27 04:50:55 +0800

    #优化随机点

--- maze/maze.py
+++ maze/maze.py
@@ -4,15 +4,14 @@ import copy
 
 
 class Map():
-    def __init__(self, start_pos, end_pos, width=16, height=16, turn=3):
+    def __init__(self, start_pos, end_pos, width=25, height=25):
         self.start_pos = Pos(start_pos[0], start_pos[1])
         self.end_pos = Pos(end_pos[0], end_pos[1])
         self.width = width
         self.height = height
-        self.turn = turn
         self.arr = [[MapStatus.empty] * width for i in xrange(height)]
 
-        self.create_path()
+        # self.create_path()
 
     def show_map(self):
         for y in xrange(self.height - 1, -1, -1):
@@ -37,7 +36,6 @@ class Map():
         # if self.arr[x][y] != MapStatus.empty: return None
         next_pos = Pos(x, y)
         if next_pos in path: return None
-
         # for key in Direction.__all__:
         #     direction = getattr(Direction, key)
         #     x = cur_pos.x + direction[0]
@@ -50,7 +48,6 @@ class Map():
             return None
         if cur_pos == ended_pos:
             self.dig_path(path)
-            # self.show_map()
             return path
 
         dir_list = copy.copy(Direction.__all__)
@@ -68,20 +65,6 @@ class Map():
 
             return self.search_path(next_pos, ended_pos, path, deep + 1,
                                     max_deep)
-            # result = self.search_path(next_pos, ended_pos, path, deep + 1, max_deep)
-            # if result is None:continue
-            # return result
-
-            # rpath, rdeep = self.search_path(next_pos, ended_pos, path, deep + 1, max_deep)
-            # print rpath, rdeep
-            # if rpath is None: continue
-            # if rdeep < tmp_deep:
-            #     tmp_path, tmp_deep = rpath, rdeep
-
-            # if tmp_path is None:
-            #     return None
-            # else:
-            #     return tmp_path, tmp_deep
         return None
 
 
@@ -90,29 +73,35 @@ class Map():
         end_pos = self.end_pos
         self.dig_pos(start_pos) 
         self.dig_pos(end_pos) 
-        while start_pos != end_pos:
-            range_width = (min(start_pos.x, end_pos.x),
-                           max(start_pos.x, end_pos.x))
-            range_height = (min(start_pos.y, end_pos.y),
-                            max(start_pos.y, end_pos.y))
-            pos_a = Pos(
-                random.randint(range_width[0], range_width[1]),
-                random.randint(range_height[0], range_height[1]))
-            pos_b = Pos(
-                random.randint(range_width[0], range_width[1]),
-                random.randint(range_height[0], range_height[1]))
 
-            # print pos_a, pos_b
+        while start_pos != end_pos:
+            rand = random.randrange(100) % 5
+            if rand == 0:
+                range_width = range(self.width)
+                range_height = range(self.height)
+            elif rand == 1:
+                range_width = range(min(start_pos.x, end_pos.x))
+                range_width.extend(range(max(start_pos.x, end_pos.x), self.width))
+                range_height = range(min(start_pos.y, end_pos.y))
+                range_height.extend(range(max(start_pos.y, end_pos.y), self.height))
+            else:
+                range_width = range(min(start_pos.x, end_pos.x),max(start_pos.x, end_pos.x))
+                range_height = range(min(start_pos.y, end_pos.y),max(start_pos.y, end_pos.y))
+
+            if len(range_width) is 0 : range_width = [start_pos.x]
+            if len(range_height) is 0 : range_height = [start_pos.y]
+
+            pos_a = Pos(
+                random.choice(range_width),
+                random.choice(range_height))
+            pos_b = Pos(
+                random.choice(range_width),
+                random.choice(range_height))
+
             if start_pos - pos_a <= start_pos - pos_b:
                 pos_a, pos_b = pos_b, pos_a
             if end_pos - pos_b <= end_pos - pos_a:
                 pos_a, pos_b = pos_b, pos_a
-
-            # self.dig_pos(pos_a) 
-            # self.dig_pos(pos_b) 
-            # self.show_map()
-            # start_pos = pos_a
-            # end_pos = pos_b
 
             path = self.search_path(start_pos, pos_a, [], 0, (start_pos - pos_a) * 2)
             if path is not None:
@@ -124,7 +113,11 @@ class Map():
                 self.dig_path(path)
                 end_pos = pos_b
 
-        self.show_map()
 
-
-Map((0, 0), (15, 15))
+a = Map((0, 0), (24, 24))
+# a = Map((0, 12), (24, 12))
+a.create_path()
+a.create_path()
+a.show_map()
+# Map((0, 12), (24, 12))
+# Map((random.randrange(16), random.randrange(16)), (random.randrange(16), random.randrange(16)))

a31a729f769d25f4aaf6c3034b8ea3053c8811c7
Floyda <floyda@163.com>
2016-03-27 04:08:33 +0800

    v0.0.1

--- maze/maze.py
+++ maze/maze.py
@@ -2,41 +2,129 @@ from utils import *
 import random
 import copy
 
-def init_map(width, height):
-    global map_width, map_height
-    map_width, map_height = width, height
 
-    return [[MapStatus.empty] * width for i in xrange(height)]
+class Map():
+    def __init__(self, start_pos, end_pos, width=16, height=16, turn=3):
+        self.start_pos = Pos(start_pos[0], start_pos[1])
+        self.end_pos = Pos(end_pos[0], end_pos[1])
+        self.width = width
+        self.height = height
+        self.turn = turn
+        self.arr = [[MapStatus.empty] * width for i in xrange(height)]
 
-def _get_pos(cur_pos, direction):
-    global map_width, map_height
-    x = cur_pos.x + direction[0]
-    if x < 0 or x > map_width : return None
-    y = cur_pos.y + direction[1]
-    if y < 0 or y > map_height : return None
-    return x, y
+        self.create_path()
 
-def _search_path(arr, pos, path):
-    rand_list = copy.copy(Direction.__all__)
-    random.shuffle(rand_list)
-    for key in rand_list:
-        new_pos = _get_pos(pos, getattr(Direction, key))
-        if new_pos is None : continue
-        print new_pos
-    return '===='
+    def show_map(self):
+        for y in xrange(self.height - 1, -1, -1):
+            s = ''
+            for x in xrange(self.width):
+                s += str(self.arr[x][y])
+            print s
+        print
 
-def generate_path(arr, start_pos, end_pos):
-    """
-    type arr       : Array map
-    type start_pos : Pos start
-    type end_pos   : Pos end
-    rtype: 
-        Array map
-        List path
-    """
-    return _search_path(copy.copy(arr), start_pos, [])
+    def dig_path(self, path, status=MapStatus.path):
+        for pos in path:
+            self.dig_pos(pos, status=status)
 
-print generate_path(init_map(3,4), Pos(0,0), Pos(2,3))
+    def dig_pos(self, pos, status=MapStatus.path):
+        self.arr[pos.x][pos.y] = status
+
+    def can_dig_next(self, cur_pos, direction, path):
+        x = cur_pos.x + direction[0]
+        if x < 0 or x >= self.width: return None
+        y = cur_pos.y + direction[1]
+        if y < 0 or y >= self.height: return None
+        # if self.arr[x][y] != MapStatus.empty: return None
+        next_pos = Pos(x, y)
+        if next_pos in path: return None
+
+        # for key in Direction.__all__:
+        #     direction = getattr(Direction, key)
+        #     x = cur_pos.x + direction[0]
+        #     y = cur_pos.y + direction[1]
+        #     if self.arr[x][y] == MapStatus.path: return None
+        return next_pos
+
+    def search_path(self, cur_pos, ended_pos, path, deep, max_deep):
+        if deep >= max_deep:
+            return None
+        if cur_pos == ended_pos:
+            self.dig_path(path)
+            # self.show_map()
+            return path
+
+        dir_list = copy.copy(Direction.__all__)
+        random.shuffle(dir_list)
+
+        tmp_path, tmp_deep = None, max_deep
+
+        for key in dir_list:
+            next_pos = self.can_dig_next(cur_pos, getattr(Direction, key),
+                                         path)
+            if next_pos is None: continue
+
+            path = copy.copy(path)
+            path.append(next_pos)
+
+            return self.search_path(next_pos, ended_pos, path, deep + 1,
+                                    max_deep)
+            # result = self.search_path(next_pos, ended_pos, path, deep + 1, max_deep)
+            # if result is None:continue
+            # return result
+
+            # rpath, rdeep = self.search_path(next_pos, ended_pos, path, deep + 1, max_deep)
+            # print rpath, rdeep
+            # if rpath is None: continue
+            # if rdeep < tmp_deep:
+            #     tmp_path, tmp_deep = rpath, rdeep
+
+            # if tmp_path is None:
+            #     return None
+            # else:
+            #     return tmp_path, tmp_deep
+        return None
 
 
+    def create_path(self):
+        start_pos = self.start_pos
+        end_pos = self.end_pos
+        self.dig_pos(start_pos) 
+        self.dig_pos(end_pos) 
+        while start_pos != end_pos:
+            range_width = (min(start_pos.x, end_pos.x),
+                           max(start_pos.x, end_pos.x))
+            range_height = (min(start_pos.y, end_pos.y),
+                            max(start_pos.y, end_pos.y))
+            pos_a = Pos(
+                random.randint(range_width[0], range_width[1]),
+                random.randint(range_height[0], range_height[1]))
+            pos_b = Pos(
+                random.randint(range_width[0], range_width[1]),
+                random.randint(range_height[0], range_height[1]))
 
+            # print pos_a, pos_b
+            if start_pos - pos_a <= start_pos - pos_b:
+                pos_a, pos_b = pos_b, pos_a
+            if end_pos - pos_b <= end_pos - pos_a:
+                pos_a, pos_b = pos_b, pos_a
+
+            # self.dig_pos(pos_a) 
+            # self.dig_pos(pos_b) 
+            # self.show_map()
+            # start_pos = pos_a
+            # end_pos = pos_b
+
+            path = self.search_path(start_pos, pos_a, [], 0, (start_pos - pos_a) * 2)
+            if path is not None:
+                self.dig_path(path)
+                start_pos = pos_a
+
+            path = self.search_path(end_pos, pos_b, [], 0, (end_pos - pos_b) * 2)
+            if path is not None:
+                self.dig_path(path)
+                end_pos = pos_b
+
+        self.show_map()
+
+
+Map((0, 0), (15, 15))
--- maze/utils.py
+++ maze/utils.py
@@ -1,18 +1,32 @@
 class MapStatus():
-    empty = 0
-    path = 1
+    # empty = 0
+    # path = 1
+    empty = '.'
+    path = 'o'
 
-class Pos():
+
+class Pos(object):
+    _instance = {}
+    def __new__(cls, x, y):
+        pos = '{x}-{y}'.format(x = x, y = y)
+        if not pos in cls._instance:
+            cls._instance[pos] = super(Pos, cls).__new__(cls, x, y)
+        return cls._instance[pos]
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
     def __str__(self):
-        return 'x = {}, y = {}'.format(self.x, self.y)
+        return '(x = {}, y = {}) '.format(self.x, self.y)
+
+    def __sub__(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
 
 class Direction():
     __all__ = ['up', 'down', 'left', 'right']
-    up    = (0, 1)
-    down  = (0, -1)
-    left  = (-1, 0)
+    up = (0, 1)
+    down = (0, -1)
+    left = (-1, 0)
     right = (1, 0)
